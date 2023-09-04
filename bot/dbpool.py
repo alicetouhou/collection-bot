ac0ed6a24544ea1ee9cb415c765de5d4337a82ca
Lunarmagpie <bambolambo0@gmail.com>
2023-09-04 19:47:42 -0400

    trade rewrite

--- bot/dbpool.py
+++ bot/dbpool.py
@@ -3,13 +3,20 @@ import psycopg2.pool
 from contextlib import contextmanager
 import os
 
-dbpool = psycopg2.pool.ThreadedConnectionPool(database="characters",
-                                            host="localhost",
-                                            user="postgres",
-                                            password=os.environ["DB_PASSWORD"],
-                                            port="5432",
-                                            minconn=0,
-                                            maxconn=100)
+dbpool: psycopg2.pool.ThreadedConnectionPool
+if passwd := os.environ.get("DB_PASSWORD"):
+    dbpool = psycopg2.pool.ThreadedConnectionPool(
+        database="characters",
+        host="localhost",
+        user="postgres",
+        password=passwd,
+        port="5432",
+        minconn=0,
+        maxconn=100,
+    )
+else:
+    dbpool = None  # type: ignore
+
 
 @contextmanager
 def db_cursor():
@@ -24,9 +31,18 @@ def db_cursor():
     finally:
         dbpool.putconn(conn)
 
-with db_cursor() as cur:
-    cur.execute("CREATE TABLE IF NOT EXISTS servers (ID varchar(31), PRIMARY KEY (ID))")
-    cur.execute("CREATE TABLE IF NOT EXISTS players_583039543893295127 (ID varchar(31), characters varchar(65535), currency int, claims int, claimed_daily int, rolls int, claimed_rolls int, wishlist varchar(1027), upgrades varchar(2055), PRIMARY KEY (ID))")
-    cur.execute("INSERT INTO servers VALUES (583039543893295127) ON CONFLICT DO NOTHING")
-    cur.execute("CREATE TABLE IF NOT EXISTS players_556614414153809961 (ID varchar(31), characters varchar(65535), currency int, claims int, claimed_daily int, rolls int, claimed_rolls int, wishlist varchar(1027), upgrades varchar(2055), PRIMARY KEY (ID))")
-    cur.execute("INSERT INTO servers VALUES (556614414153809961) ON CONFLICT DO NOTHING")
+if dbpool:
+    with db_cursor() as cur:
+        cur.execute("CREATE TABLE IF NOT EXISTS servers (ID varchar(31), PRIMARY KEY (ID))")
+        cur.execute(
+            "CREATE TABLE IF NOT EXISTS players_583039543893295127 (ID varchar(31), characters varchar(65535), currency int, claims int, claimed_daily int, rolls int, claimed_rolls int, wishlist varchar(1027), upgrades varchar(2055), PRIMARY KEY (ID))"
+        )
+        cur.execute(
+            "INSERT INTO servers VALUES (583039543893295127) ON CONFLICT DO NOTHING"
+        )
+        cur.execute(
+            "CREATE TABLE IF NOT EXISTS players_556614414153809961 (ID varchar(31), characters varchar(65535), currency int, claims int, claimed_daily int, rolls int, claimed_rolls int, wishlist varchar(1027), upgrades varchar(2055), PRIMARY KEY (ID))"
+        )
+        cur.execute(
+            "INSERT INTO servers VALUES (556614414153809961) ON CONFLICT DO NOTHING"
+        )
