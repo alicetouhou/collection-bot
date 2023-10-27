43d72a099c56430773dfe67d5a0e2ef4812675a9
Alice <63879236+Benji42@users.noreply.github.com>
2023-10-27 05:23:11 -0400

    Fix that stupid bug (and other small changes)

--- bot/plugins/list.py
+++ bot/plugins/list.py
@@ -62,7 +62,7 @@ class ListCommand:
 
         if self.search:
             char_filter = await dbsearch.create_character_from_search(
-                ctx.guild_id, self.search, limit=100)
+                ctx.guild_id, self.search, limit=None)
             new_list = filter(
                 lambda char: char in char_filter, character_list)
             character_list = list(new_list)
